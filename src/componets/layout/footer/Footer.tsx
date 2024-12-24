81133c15a88e41edca5c6ad25ced605bac2763bd
leecheonguk <dlcjsrnr0860@naver.com>
2024-12-25 00:14:30 +0900

    footer

--- src/componets/layout/footer/Footer.tsx
+++ src/componets/layout/footer/Footer.tsx
@@ -3,13 +3,17 @@ import Paths from '../../../utils/paths';
 import logo1 from '/src/assets/KakaoTalk_20241224_233352782.png';
 import logo2 from '/src/assets/KakaoTalk_20241224_233552424.png';
 import { Link } from 'react-router-dom';
+import useIsMobile from '../../../hooks/useIsMobile';
 
-const FooterWrapper = styled.footer`
-  display: table-column;
+const FooterWrapper = styled.footer<{ isMobile: boolean }>`
+  display: ${(props) => (props.isMobile ? 'flex' : 'table-column')};
+  flex-direction: ${(props) => (props.isMobile ? 'column' : 'initial')};
+  justify-content: ${(props) => (props.isMobile ? 'center' : 'initial')};
+  align-items: ${(props) => (props.isMobile ? 'center' : 'initial')};
   background-color: #333;
   color: white;
   padding: 20px;
-  text-align: left;
+  text-align: ${(props) => (props.isMobile ? 'center' : 'left')};
   height: 280px;
 `;
 
@@ -24,24 +28,27 @@ const StyledLink = styled(Link)`
 `;
 
 const Footer = () => {
+  const isMobile = useIsMobile();
   return (
-    <FooterWrapper>
+    <FooterWrapper isMobile={isMobile}>
       <img
         src={logo1}
         style={{
           width: '180px',
-          height: '50px' /* 이미지 높이를 제한 */,
+          height: '50px',
           objectFit: 'cover' /* 이미지가 잘리도록 설정 */,
-          marginLeft: '50px',
+          marginBottom: isMobile
+            ? '10px'
+            : '0' /* 모바일에서 이미지를 위아래로 약간 떨어지게 함 */,
         }}
       />
-      <div style={{ marginLeft: '5%' }}>
+      <div style={{ textAlign: isMobile ? 'center' : 'left' }}>
         <p>(주) 럽앤굿 모먼트 | 대표 : 민인애</p>
         <p>개인정보보호책임자 : 유선주</p>
         <p>E-MAIL : love.goodmoment@gmail.com</p>
         <p>&copy; 2024 dikaiosyne. All Rights Reserved.</p>
         <p>
-          <div style={{ display: 'flex' }}>
+          <div style={{ display: 'flex', justifyContent: 'left' }}>
             <StyledLink to={Paths.Privacy}>privacy policy</StyledLink>
             <div style={{ width: '10px' }} />
             <StyledLink to={Paths.TermsOfUse}>terms of use</StyledLink>
@@ -52,9 +59,9 @@ const Footer = () => {
         src={logo2}
         style={{
           width: '200px',
-          height: '60px' /* 이미지 높이를 제한 */,
-          objectFit: 'cover' /* 이미지가 잘리도록 설정 */,
-          marginLeft: '50px',
+          height: '60px',
+          objectFit: 'cover',
+          marginTop: isMobile ? '10px' : '0',
         }}
       />
     </FooterWrapper>
