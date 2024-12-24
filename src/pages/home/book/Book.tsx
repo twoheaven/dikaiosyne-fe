def7beb53e7ae7581b9f3c8b18682c2faf6f5298
leecheonguk <dlcjsrnr0860@naver.com>
2024-12-24 22:42:20 +0900

    book 업데이트트

--- src/pages/home/book/Book.tsx
+++ src/pages/home/book/Book.tsx
@@ -5,12 +5,12 @@ import Card from '../../../componets/feature/card/Card';
 import Paths from '../../../utils/paths';
 import useIsMobile from '../../../hooks/useIsMobile';
 
-const BookWrapper = styled.div`
+const BookWrapper = styled.div<{ isMobile: boolean }>`
   display: flex;
   text-align: center;
   align-items: center;
   justify-content: center;
-  aspect-ratio: 2;
+  aspect-ratio: ${(props) => (props.isMobile ? '2' : '2.5/1')};
   background-color: wheat;
   width: 100%;
 `;
@@ -23,7 +23,7 @@ const CardsContainer = styled.div<{ isMobile: boolean }>`
   align-items: center;
   padding: ${(props) => (props.isMobile ? '30px' : '')};
   gap: ${(props) =>
-    props.isMobile ? '30px' : '3%'}; /* Mobile: no gap, PC: 16px */
+    props.isMobile ? '30px' : '5%'}; /* Mobile: no gap, PC: 16px */
 `;
 
 const Book: React.FC = () => {
